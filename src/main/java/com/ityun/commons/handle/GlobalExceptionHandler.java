5cc43bbe1c4a036dcdd6beda6ec4d9b6ae09acdd
changyuyun <969842624@qq.com>
2020-04-10 18:28:04 +0800

    完善global exception捕捉

--- src/main/java/com/ityun/commons/handle/GlobalExceptionHandler.java
+++ src/main/java/com/ityun/commons/handle/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.ityun.commons.handle;
 
 import com.ityun.response.ResponseNoData;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +27,30 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseNoData handleConstraintViolationException(ConstraintViolationException e) {
         String message = e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(","));
+        return ResponseNoData.error(-1, message);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseNoData handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseNoData.error(-1, message);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseNoData handelHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        String message = e.getMessage();
+        return ResponseNoData.error(-1, message);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseNoData handelMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        String message = e.getMessage();
+        return ResponseNoData.error(-1, message);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseNoData handelHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        String message = e.getMessage();
         return ResponseNoData.error(-1, message);
     }
 }
--- src/main/java/com/ityun/controller/TestController.java
+++ src/main/java/com/ityun/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.ityun.controller;
 
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
 
@@ -11,6 +10,11 @@ import javax.validation.constraints.NotBlank;
 public class TestController {
     @GetMapping("/test")
     public String doTest(@NotBlank(message = "id不能空") String id, @NotBlank(message = "username不能为空") String username) {
-        return "test";
+        return "test get";
+    }
+
+    @PostMapping("/test2")
+    public String doTest2(@RequestBody @RequestParam @RequestAttribute @NotBlank(message = "id不能空") String id, @NotBlank(message = "username不能为空") String username) {
+        return "test post";
     }
 }
