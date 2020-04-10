ec5247e2284b0427191beea9399d4363cca749cd
changyuyun <969842624@qq.com>
2020-04-10 21:01:35 +0800

    增加获取签名的工具类: TODO

---
+++ src/main/java/com/ityun/utils/SignUtils.java
@@ -0,0 +1,22 @@
+package com.ityun.utils;
+
+/**
+ * 签名工具类
+ */
+public class SignUtils {
+    /**
+     * 构造签名
+     * @return
+     */
+    public String getSign() {
+        return "";
+    }
+
+    /**
+     * 验签
+     * @return
+     */
+    public boolean verifySign() {
+        return false;
+    }
+}
